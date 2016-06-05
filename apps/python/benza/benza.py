c4d74090f20edd658bb97e9946ff398c8f18ba93
bernardodestefano <bernardo.destefano@gmail.com>
2016-06-05 23:21:19 +0200

    code's cleaning

--- apps/python/benza/benza.py
+++ apps/python/benza/benza.py
@@ -3,27 +3,25 @@ import ac
 import acsys
 import sim_info
 
-#variabili globali poiche ci servono sia un acMain che in acUpdate
 l_lapcount=0
 lapcount=0
 b_benzina=0
 benzina=0
 
-def acMain(ac_version): # acMain si occupa esclusivamente del setup dell'app
+def acMain(ac_version):
     global l_lapcount, b_benzina
 
 
-    appWindow = ac.newApp("benza") #finestra effettiva dell'app
+    appWindow = ac.newApp("benza")
     ac.setSize(appWindow, 200, 200)
 
-    ac.log("Hello, Assetto Corsa application world!")
-    ac.console("Hello, Assetto Corsa console!")
+    ac.log("Benza Start")
+    ac.console("Benza is running")
 
     l_lapcount = ac.addLabel(appWindow, "Laps: 0");
     b_benzina  = ac.addLabel(appWindow, "Fuel: 0");
 
-    ac.setPosition(l_lapcount, 3, 30) #il label sara settato a 30 verticali (poiche in alto c'e l'header) e 3 orizzontali per separarlo dal bordo
-    ac.setPosition(b_benzina, 3, 50 )
+    ac.setPosition(l_lapcount, 3, 30)
 
     return "benza"
 
@@ -43,5 +41,5 @@ def acUpdate(deltaT):
         ac.setText(b_benzina, "Fuel: {}".format(benzina))
 
 def acShutdown():
-    ac.log("app benz chiusa")
-    ac.console("app benza chiusa")
+    ac.log("Benza exit")
+    ac.console("Benza exit")
